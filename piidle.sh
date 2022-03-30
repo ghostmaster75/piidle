b4b18c722101e23d22ebaeca35063dd06646aba3
Pier <ghostmaster75@gmail.com>
2022-03-30 02:48:41 +0200

    Update piidle.sh

--- piidle.sh
+++ piidle.sh
@@ -10,20 +10,27 @@ BMIN=15
 BMAX=255
 triggered=false
 
+#FOR RASPBIAN BULLEYE
+BPATH='/sys/class/backlight/10-0045/brightness'
+
+#FOR RASPBIAN BUSTER
+#BPATH='/sys/class/backlight/rpi_backlight/brightness'
+
+
 usage() {
-	printf '\nUse : piidle [option]\n'
-	printf '\nHelp: \n'
-	printf '\n-i, --idle\t\tIdle Time in seconds. Default value is 30 secods'
-	printf '\n-bmin, --brightness-min\tMinimun value of brightness.\n\t\t\tValid value are from 0 to 255'
-	printf '\n-bmax, --brightness-max\tMaximun value of brightness.\n\t\t\tValid value are from 0 to 255'
-	printf '\n-r, --reset\t\tReset birghtness to 255'
-	printf '\n-h, --help\t\tthis help'
-	printf '\n\n'
+        printf '\nUse : piidle [option]\n'
+        printf '\nHelp: \n'
+        printf '\n-i, --idle\t\tIdle Time in seconds. Default value is 30 secods'
+        printf '\n-bmin, --brightness-min\tMinimun value of brightness.\n\t\t\tValid value are from 0 to 255'
+        printf '\n-bmax, --brightness-max\tMaximun value of brightness.\n\t\t\tValid value are from 0 to 255'
+        printf '\n-r, --reset\t\tReset birghtness to 255'
+        printf '\n-h, --help\t\tthis help'
+        printf '\n\n'
 }
 
 reset() {
-	printf 'Reset brightness\n'
-	eval $(sudo sh -c 'echo 255 > /sys/class/backlight/10-0045/brightness')
+        printf 'Reset brightness\n'
+        eval $(sudo sh -c 'echo 255 > "$BPATH"')
 }
 
 prerequisite() {
@@ -34,6 +41,7 @@ prerequisite() {
                 echo "No $REQUIRED_PKG. Please install with: 'sudo apt install $REQUIRED_PKG' "
                 exit 1
         fi
+        printf 'path : %s\n' $BPATH
 }
 
 while true; do
@@ -51,8 +59,8 @@ while true; do
       usage
       exit 0;;
      -r|--reset)
-	reset
-	exit 0;;
+        reset
+        exit 0;;
     --)
       break;;
      *)
@@ -71,46 +79,39 @@ fi
 
 if ! [ "$BMIN" -eq "$BMIN" ] 2>/dev/null
 then
-	printf "${RED}ERROR: Invalid value of brightness-min${NC}\n"
-	usage
-	exit 1
-fi
-
-if ! [ "$BMAX" -eq "$BMAX" ] 2>/dev/null
-then
-        printf "${RED}ERROR: Invalid value of brightness-max${NC}\n"
-	usage
+        printf "${RED}ERROR: Invalid value of brightness-min${NC}\n"
+        usage
         exit 1
 fi
 
 if [ $BMIN -ge $BMAX ]
 then
-	printf "${RED}ERROR: brightness-max must be greater than brightness-min${NC}\n"
-	usage
-	exit 1
+        printf "${RED}ERROR: brightness-max must be greater than brightness-min${NC}\n"
+        usage
+        exit 1
 fi
 
 if [ $BMIN -gt 255 ] || [ $BMIN -lt 0 ]
 then
-	printf "${RED}ERROR: brightness-min out of range.\nValid value are from 0 to 255${NC}\n"
-	usage
-	exit 1
+        printf "${RED}ERROR: brightness-min out of range.\nValid value are from 0 to 255${NC}\n"
+        usage
+        exit 1
 fi
 
 if [ $BMAX -gt 255 ] || [ $BMAX -lt 0 ]
 then
-	printf "${RED}ERROR: brightness-max out of range.\nValid value are from 0 to 255${NC}\n"
-	exit 1
+        printf "${RED}ERROR: brightness-max out of range.\nValid value are from 0 to 255${NC}\n"
+        exit 1
 fi
 
-printf "PIIDLE : IDLE: %s - BMIN: %s - BMAX: %s" "$IDLE" "$BMIN" "$BMAX"
+printf "PIIDLE : IDLE: %s - BMIN: %s - BMAX: %s" "$IDLE" "$BMIN" "$BMAX\n"
 
 timeout=$(($IDLE*1000))
 
 while true
 do
     tosleep=$(((timeout - $(xssstate -i)) / 1000))
-    brightness=$(cat /sys/class/backlight/10-0045/brightness)
+    brightness=$(cat "$BPATH")
     if [ $tosleep -le 0 ];
     then
         while [ $brightness -ge $BMIN ]; do
@@ -119,22 +120,22 @@ do
            then
                 break
            fi
-           $triggered || eval $(sudo sh -c 'echo '"$brightness"' > /sys/class/backlight/10-0045/brightness')
+           $triggered || eval $(sudo sh -c 'echo '"$brightness"' > '"$BPATH")
            brightness=$(($brightness-3))
         done
         tosleep=$(((timeout - $(xssstate -i)) / 1000))
         if [ $tosleep -le 0 ]
         then
-                $triggered || eval $(sudo sh -c 'echo '"$BMIN"' > /sys/class/backlight/10-0045/brightness')
+                $triggered || eval $(sudo sh -c 'echo '"$BMIN"' > '"$BPATH")
         fi
         triggered=true
     else
         triggered=false
         while [ $brightness -le $BMAX ]; do
-                $triggered || eval $(sudo sh -c 'echo '"$brightness"' > /sys/class/backlight/10-0045/brightness')
+                $triggered || eval $(sudo sh -c 'echo '"$brightness"' > '"$BPATH")
                 brightness=$(($brightness+3))
         done
-        $triggered || eval $(sudo sh -c 'echo '"$BMAX"' > /sys/class/backlight/10-0045/brightness')
+        $triggered || eval $(sudo sh -c 'echo '"$BMAX"' > '"$BPATH")
         sleep $tosleep
     fi
-done
+done	
