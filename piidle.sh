13be8ac66c8062bb5eaf96847244c065b40da6e6
Pier <ghostmaster75@gmail.com>
2022-04-04 01:29:33 +0200

    Update piidle.sh
    add disable fade effect arguments

--- piidle.sh
+++ piidle.sh
@@ -5,32 +5,37 @@ NC='\e[0m'
 GREEN='\e[0;32m'
 DGRAY='\e[0;30m'
 BLUE='\e[1;34m'
-IDLE=30
-BMIN=15
+IDLE=60
+BMIN=80
 BMAX=255
 triggered=false
+FADEIN=true
+FADEOUT=true
 
 #FOR RASPBIAN BULLEYE
-BPATH='/sys/class/backlight/10-0045/brightness'
+#BPATH='/sys/class/backlight/10-0045/brightness'
 
 #FOR RASPBIAN BUSTER
-#BPATH='/sys/class/backlight/rpi_backlight/brightness'
+BPATH='/sys/class/backlight/rpi_backlight/brightness'
 
 
 usage() {
-        printf '\nUse : piidle [option]\n'
-        printf '\nHelp: \n'
-        printf '\n-i, --idle\t\tIdle Time in seconds. Default value is 30 secods'
-        printf '\n-bmin, --brightness-min\tMinimun value of brightness (default is 15).\n\t\t\tValid value are from 0 to 255'
-        printf '\n-bmax, --brightness-max\tMaximun value of brightness (default is 255).\n\t\t\tValid value are from 0 to 255'
-        printf '\n-r, --reset\t\tReset birghtness to 255'
-        printf '\n-h, --help\t\tthis help'
-        printf '\n\n'
+	printf '\nUse : piidle [option]\n'
+	printf '\nHelp: \n'
+	printf '\n-i, --idle\t\tIdle Time in seconds. Default value is 30 secods'
+	printf '\n-bmin, --brightness-min\tMinimun value of brightness.\n\t\t\tValid value are from 0 to 255. Default value is 20'
+	printf '\n-bmax, --brightness-max\tMaximun value of brightness.\n\t\t\tValid value are from 0 to 255. Default value is 255'
+	printf '\n-nfi, --no-fade-in\tDisable fade effect on idle'
+	printf '\n-nfo, --no-fade-out\tDisable fade on resume'
+	printf '\n-nf, --no-fade\t\tDisable fade effect'
+	printf '\n-r, --reset\t\tReset birghtness to 255'
+	printf '\n-h, --help\t\tthis help'
+	printf '\n\n'
 }
 
 reset() {
-        printf 'Reset brightness\n'
-        eval $(sudo sh -c 'echo 255 > '"$BPATH")
+	printf 'Reset brightness\n'
+	eval $(sudo sh -c 'echo 255 > '"$BPATH")
 }
 
 prerequisite() {
@@ -41,7 +46,7 @@ prerequisite() {
                 echo "No $REQUIRED_PKG. Please install with: 'sudo apt install $REQUIRED_PKG' "
                 exit 1
         fi
-        printf 'path : %s\n' $BPATH
+	printf 'path : %s\n' $BPATH
 }
 
 while true; do
@@ -55,14 +60,26 @@ while true; do
     -i|--idle)
       IDLE="$2"
       shift 2;;
+    -nfi|--no-fade-in)
+      FADEIN=false
+      shift;;
+    -nfo|--no-fade-out)
+      FADEOUT=false
+      shift;;
+    -nf|--no-fade)
+      FADEIN=false
+      FADEOUT=false
+      shift;;
     -h|--help)
       usage
       exit 0;;
-     -r|--reset)
-        reset
-        exit 0;;
-    --)
-      break;;
+    -r|--reset)
+      reset
+      exit 0;;
+     --)
+      printf "/nInvalid argument $1/n"
+      usage
+      exit 1;;
      *)
       break;;
   esac
@@ -79,29 +96,36 @@ fi
 
 if ! [ "$BMIN" -eq "$BMIN" ] 2>/dev/null
 then
-        printf "${RED}ERROR: Invalid value of brightness-min${NC}\n"
-        usage
+	printf "${RED}ERROR: Invalid value of brightness-min${NC}\n"
+	usage
+	exit 1
+fi
+
+if ! [ "$BMAX" -eq "$BMAX" ] 2>/dev/null
+then
+        printf "${RED}ERROR: Invalid value of brightness-max${NC}\n"
+	usage
         exit 1
 fi
 
 if [ $BMIN -ge $BMAX ]
 then
-        printf "${RED}ERROR: brightness-max must be greater than brightness-min${NC}\n"
-        usage
-        exit 1
+	printf "${RED}ERROR: brightness-max must be greater than brightness-min${NC}\n"
+	usage
+	exit 1
 fi
 
 if [ $BMIN -gt 255 ] || [ $BMIN -lt 0 ]
 then
-        printf "${RED}ERROR: brightness-min out of range.\nValid value are from 0 to 255${NC}\n"
-        usage
-        exit 1
+	printf "${RED}ERROR: brightness-min out of range.\nValid value are from 0 to 255${NC}\n"
+	usage
+	exit 1
 fi
 
 if [ $BMAX -gt 255 ] || [ $BMAX -lt 0 ]
 then
-        printf "${RED}ERROR: brightness-max out of range.\nValid value are from 0 to 255${NC}\n"
-        exit 1
+	printf "${RED}ERROR: brightness-max out of range.\nValid value are from 0 to 255${NC}\n"
+	exit 1
 fi
 
 printf "PIIDLE : IDLE: %s - BMIN: %s - BMAX: %s" "$IDLE" "$BMIN" "$BMAX\n"
@@ -114,15 +138,18 @@ do
     brightness=$(cat "$BPATH")
     if [ $tosleep -le 0 ];
     then
-        while [ $brightness -ge $BMIN ]; do
-           tosleep=$(((timeout - $(xssstate -i)) / 1000))
-           if [ $tosleep -gt 0 ]
-           then
-                break
-           fi
-           $triggered || eval $(sudo sh -c 'echo '"$brightness"' > '"$BPATH")
-           brightness=$(($brightness-3))
-        done
+	if $FADEIN
+	then
+           while [ $brightness -ge $BMIN ]; do
+              tosleep=$(((timeout - $(xssstate -i)) / 1000))
+              if [ $tosleep -gt 0 ]
+              then
+                   break
+              fi
+              $triggered || eval $(sudo sh -c 'echo '"$brightness"' > '"$BPATH")
+              brightness=$(($brightness-3))
+           done
+        fi
         tosleep=$(((timeout - $(xssstate -i)) / 1000))
         if [ $tosleep -le 0 ]
         then
@@ -131,11 +158,14 @@ do
         triggered=true
     else
         triggered=false
-        while [ $brightness -le $BMAX ]; do
-                $triggered || eval $(sudo sh -c 'echo '"$brightness"' > '"$BPATH")
-                brightness=$(($brightness+3))
-        done
+	if $FADEOUT
+	then
+           while [ $brightness -le $BMAX ]; do
+                   $triggered || eval $(sudo sh -c 'echo '"$brightness"' > '"$BPATH")
+                   brightness=$(($brightness+3))
+           done
+        fi
         $triggered || eval $(sudo sh -c 'echo '"$BMAX"' > '"$BPATH")
         sleep $tosleep
     fi
-done	
+done
